d18b15a49b55e5575bb43bb62f1452e49f5e3936
Test <Test@GWS001.local>
2019-08-22 10:33:09 +0300

    EAccessViolation issue

--- dbmanager.pas
+++ dbmanager.pas
@@ -32,25 +32,29 @@ procedure TDBMgr.LoadConfig;
 var
   i,j: longint;
 begin
-  // Load Config
-  fQuery.SQL.Text:='select * from '+db_conf;
-  fQuery.Open;
-  i:=0;
-  // Fetching
+  // Load Config                  
   /// TODO: try/except must be added
-  SetLength(ConfigLines,fQuery.RecordCount);
-  while not fQuery.EOF do begin
-    j:=0;
-    for fField in fQuery.Fields do begin
-      if fField.IsNull then ConfigLines[i,j]:='NULL'
-        else ConfigLines[i,j]:=fField.Value;
-      Inc(j);
-      end;
-    fQuery.Next;
-    Inc(i);
+  try
+    fQuery.SQL.Text:='select * from '+db_conf;
+    fQuery.Open;
+    i:=0;
+    // Fetching
+    SetLength(ConfigLines,fQuery.RecordCount);
+    while not fQuery.EOF do begin
+      j:=0;
+      for fField in fQuery.Fields do begin
+        if fField.IsNull then ConfigLines[i,j]:='NULL'
+          else ConfigLines[i,j]:=fField.Value;
+        Inc(j);
+        end;
+      fQuery.Next;
+      Inc(i);
+    end;
+    // Close Query
+    fQuery.Close;
+  except
+    WriteLn('MySQL Error at config');
   end;
-  // Close Query
-  fQuery.Close;
 
   // Fill up variables
   // 1. Mail verification fields
@@ -69,58 +73,71 @@ var
   acc: TAccount;
   pacc: PAccount;
   p: pointer;
-begin
-  fQuery.SQL.Text:='select * from '+db_accs;
-  fQuery.Open;
-  // Fetching
+begin                
   /// TODO: try/except must be added
-  while not fQuery.EOF do begin
-    Acc.id:=0;
-    Acc.acLevel:=0;
-    acc.isBanned:=false;
-    acc.isBlocked:=false;
-    acc.isOnline:=false;
-    acc.isVerified:=false;
-    acc.LastDate:='';
-    acc.LastIP:='';
-    acc.mail:='';
-    acc.pass:='';
-    acc.RegDate:='';
+  try
+    fQuery.SQL.Text:='select * from '+db_accs;
+    fQuery.Open;
+    //write('Getting mem... ');
+    //GetMem(Pacc,sizeof(acc));
+    //WriteLn('Done.');
+    // Fetching
+    while not fQuery.EOF do begin
+      Acc.id:=0;
+      Acc.acLevel:=0;
+      acc.isBanned:=false;
+      acc.isBlocked:=false;
+      acc.isOnline:=false;
+      acc.isVerified:=false;
+      acc.LastDate:='';
+      acc.LastIP:='';
+      acc.mail:='';
+      acc.pass:='';
+      acc.RegDate:='';
 
-    //@Acc:=nil;
-    for fField in fQuery.Fields do begin
-    // if not(fField.IsNull) then
-    //  if fField.IsNull then tmp[i,j]:='NULL'
-    //    else tmp[i,j]:=fField.Value;
-      Case fField.FieldName of
-        acc_ID: if not(fField.IsNull) then acc.id:=fField.Value;
-        acc_Mail:  if not(fField.IsNull) then acc.mail:=fField.Value;
-        acc_Pass: if not(fField.IsNull) then acc.pass:=fField.Value;
-        acc_isBanned: if not(fField.IsNull) then acc.isBanned:=ffield.Value;
-        acc_isBlocked: if not(fField.IsNull) then acc.isBlocked:=fField.value;
-        acc_isVerified: if not(fField.IsNull) then acc.isVerified:=ffield.Value;
-        acc_isOnline: if not(fField.IsNull) then acc.isOnline:=ffield.Value;
-        acc_LastLogin: if not(fField.IsNull) then acc.LastDate:=fField.Value;
-        acc_LastIP: if not(fField.IsNull) then acc.LastIP:=Ffield.Value;
-        acc_Registered: if not(fField.IsNull) then acc.RegDate:=fField.Value;
+      //pAcc:=nil;
+      for fField in fQuery.Fields do begin
+      // if not(fField.IsNull) then
+      //  if fField.IsNull then tmp[i,j]:='NULL'
+      //    else tmp[i,j]:=fField.Value;
+        Case fField.FieldName of
+          acc_ID: if not(fField.IsNull) then acc.id:=fField.Value;
+          acc_Mail:  if not(fField.IsNull) then acc.mail:=fField.Value;
+          acc_Pass: if not(fField.IsNull) then acc.pass:=fField.Value;
+          acc_isBanned: if not(fField.IsNull) then acc.isBanned:=ffield.Value;
+          acc_isBlocked: if not(fField.IsNull) then acc.isBlocked:=fField.value;
+          acc_isVerified: if not(fField.IsNull) then acc.isVerified:=ffield.Value;
+          acc_isOnline: if not(fField.IsNull) then acc.isOnline:=ffield.Value;
+          acc_LastLogin: if not(fField.IsNull) then acc.LastDate:=fField.Value;
+          acc_LastIP: if not(fField.IsNull) then acc.LastIP:=Ffield.Value;
+          acc_Registered: if not(fField.IsNull) then acc.RegDate:=fField.Value;
+        end;
       end;
+      WriteLn('Account "',acc.mail,'" is gathered');
+      pacc:=@acc;
+      WriteLn('Acc -> PAcc');
+      Accounts.Add(pacc);
+      WriteLn('Account added');
+      fQuery.Next;
     end;
-    WriteLn('Account "',acc.mail,'" is gathered');
-    pacc^:=acc;
-    WriteLn('//p:=Getmem(sizeof(Acc));');
-    Accounts.Add(pacc);
-    WriteLn('Account added');
-    fQuery.Next;
+    // Close Query
+    fQuery.Close;
+  except
+    //WriteLn('MySQL error at accounts');
+    on E: ESQLDatabaseError do
+      WriteLn(e.ClassName,' : ',e.Message);
+    on E: EAccessViolation do
+      WriteLn(e.ClassName, ' : ',e.Message);
+    on E: Exception do
+      WriteLn(e.ClassName,' : ',e.Message);
   end;
-  // Close Query
-  fQuery.Close;
 
   // Fill up variables
   // Accounts
-  for i:=0 to Accounts.Count-1 do
-  begin
-    WriteLn('Loaded: ',Accounts.Items[i]^.mail);
-  end;
+  //for i:=0 to Accounts.Count-1 do
+  //begin
+  //  WriteLn('Loaded: ',Accounts.Items[i]^.mail);
+  //end;
 end;
 
 constructor TDBMgr.Create;
